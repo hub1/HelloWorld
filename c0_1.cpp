3bcfba6e6d9cf5d13eec12fd84fd87e53e0b0025
hubb2 <cute_babe898@hotmail.com>
2015-04-10 19:47:04 +0300

    Update c0_1.cpp
    how is this?

--- c0_1.cpp
+++ c0_1.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 
 int main(){
+	string name;
 	//the famous test output
-	cout<<"Hello World!"<<endl; 
+	cout<<"Please insert your name: ";
+	cin>>name;
+	cout<<"Hello World!"<<name<<endl; 
 	
 	return 0;
 }
